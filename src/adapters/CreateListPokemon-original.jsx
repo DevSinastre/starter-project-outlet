b779e95327990a97b60f58a8ece63e8770b285c9
Rodrigo Sinastre <rodrigo.sinastre@gmail.com>
2023-12-21 23:27:04 -0300

    alterando dados de fetch

--- src/adapters/CreateListPokemon-original.jsx
+++ src/adapters/CreateListPokemon-original.jsx
@@ -1,5 +1,5 @@
 export default async function CreateListPokemon (count) {
-    const createListPokemon =   await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=10&offset=${count}.`)
+    const createListPokemon =   await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${count}.`)
     const response = await createListPokemon.json();
     return response.results
 }
